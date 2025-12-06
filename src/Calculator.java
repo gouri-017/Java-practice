3590ddbab5a4e7cfffc8b9a5c180e646970c5663
Gouri <gouri7136@gmail.com>
2025-12-07 00:15:25 +0530

    basic functionality works

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,13 +3,44 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("please enter two numbers.");
-        double a = sc.nextDouble();
-        double b = sc.nextDouble();
-        add(a,b);
-        subtract(a,b);
-        mul(a,b);
-        divide(a,b);
+        double a,b;
+
+        char operation = 'y';
+        do {
+            System.out.println("please enter two numbers.");
+            if (sc.hasNextDouble())
+                 a = sc.nextDouble();
+            else{
+            System.out.println("please enter valid input.");
+            sc.next();
+            continue;
+            }
+            if (sc.hasNextDouble())
+                b = sc.nextDouble();
+            else{
+                System.out.println("please enter valid input.");
+                sc.next();
+                continue;
+            }
+            System.out.println("please choose the operation to perform -: +, -, *, / ('q' to exit): ");
+            operation = sc.next().charAt(0);
+            switch (operation){
+                case '+' -> add(a,b);
+                case '-' -> subtract(a,b);
+                case '*' -> mul(a,b);
+                case '/' -> divide(a,b);
+                case 'q','Q' -> {
+                    operation = 'q';
+                    continue;
+                }
+                default -> System.out.println("Invalid Operation. Either choose again or press 'q' to exit.");
+            }
+        } while (operation != 'q');
+
+
+
+
+
     }
     static void add(double a, double b) {
         System.out.println("Sum: "+(a+b));
@@ -23,6 +54,10 @@ public class Calculator {
     }
 
     static void divide(double a, double b) {
+        if (b == 0){
+            System.out.println("Error.Cannot divide by zero.");
+            return;
+        }
         System.out.println("divide: "+(a/b));
     }
 
